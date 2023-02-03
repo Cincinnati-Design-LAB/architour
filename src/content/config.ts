854afb4d52b36af49c3625ef1aa820974b4febbc
Sean C Davis <scdavis41@gmail.com>
2023-02-02 20:42:13 -0500

    Add a handful more fields for buildings

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,27 @@
 import { z, defineCollection } from 'astro:content'
 
 const buildingsCollection = defineCollection({
-  schema: z
-    .object({
-      title: z.string(),
-    })
-    .required({ title: true }),
+  schema: z.object({
+    title: z.string().nonempty(),
+    images: z.array(z.string()),
+    location: z.object({
+      lat: z.number(),
+      lng: z.number(),
+    }),
+    address: z.string().optional(),
+    original_owner: z.string().optional(),
+    date_of_completion: z.string().optional(),
+    style: z.string().optional(),
+    architect: z.string().optional(),
+    contractor: z.string().optional(),
+    original_function: z.string().optional(),
+    significance: z.string().optional(),
+    historic_status: z.string().optional(),
+    unique_features: z.string().optional(),
+    renovations: z.string().optional(),
+    renovation_date: z.string().optional(),
+    renovation_architect: z.string().optional(),
+  }),
 })
 
 const toursCollection = defineCollection({
