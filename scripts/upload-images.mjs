83030ff38b5a8db70527f71ec2f9d627027dc2cb
Sean C Davis <scdavis41@gmail.com>
2023-02-27 06:45:49 -0500

    Simple upload example script for Cloudinary

---
+++ scripts/upload-images.mjs
@@ -0,0 +1,27 @@
+#!/usr/env/bin node
+
+/**
+ * NOTE: The Cloudinary Node.js SDK requires that CLOUDINARY_URL is set as an
+ * environment variable.
+ */
+
+// import path from 'path'
+// import { v2 as cloudinary } from 'cloudinary'
+
+// --- Upload Image ---
+//
+// const filePath = path.join(process.cwd(), 'public', 'favicon.png')
+//
+// const result = await cloudinary.uploader.upload(filePath, {
+//   folder: 'test-folder',
+//   async: true,
+//   use_filename: true,
+//   unique_filename: false,
+// })
+//
+// console.log({ result })
+
+// --- Get Image URL ---
+//
+// const url = cloudinary.url('test-folder/favicon.png', { width: 100, height: 150, crop: 'fill' })
+// console.log({ url })
--- scripts/uploadcare-export.mjs
+++ scripts/uploadcare-export.mjs
@@ -1,7 +1,6 @@
 #!/usr/env/bin node
 
 import axios from 'axios'
-import client from 'https'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
