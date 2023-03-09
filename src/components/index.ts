eb6a2f55297f154ebabb7772d757b90b14c30139
Sean C Davis <scdavis41@gmail.com>
2023-03-08 20:20:41 -0500

    A bit of styling for the tour detail

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import BuildingAttribute, { Props as BuildingAttributeProps } from './BuildingAttribute.astro'
 import BuildingCard, { Props as BuildingCardProps } from './Card/Building.astro'
 import Button, { Props as ButtonProps } from './Button.astro'
+import CompactCard, { Props as CompactCardProps } from './Card/Compact.astro'
 import Footer from './Footer.astro'
 import Header from './Header.astro'
 import HeaderLink, { Props as HeaderLinkProps } from './HeaderLink.astro'
@@ -13,6 +14,7 @@ export {
   BuildingAttribute,
   BuildingCard,
   Button,
+  CompactCard,
   Footer,
   Header,
   HeaderLink,
@@ -26,6 +28,7 @@ export type {
   BuildingAttributeProps,
   BuildingCardProps,
   ButtonProps,
+  CompactCardProps,
   HeaderLinkProps,
   IconProps,
   PageHeaderProps,
