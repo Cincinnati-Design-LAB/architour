57b9bab9868e79bb0a52a7f9bb38ddf6750f5d31
Sean C Davis <scdavis41@gmail.com>
2023-04-17 07:58:36 -0400

    Put `data_of_completion` back and remove schema comments

--- content/schema/Building.ts
+++ content/schema/Building.ts
@@ -132,91 +132,10 @@ export const Building = defineDocumentType(() => ({
       description: 'A list of attributes to be shown on the detail page. Each ',
       of: [BuildingAttributeSection, BuildingRenovationSection],
     },
-    // original_owner: {
-    //   type: 'string',
-    //   description: 'Name of the original owner, when the building was constructed.',
-    // },
-    // current_owner: {
-    //   type: 'string',
-    //   description: 'Name of the current owner of the property.',
-    // },
-    // date_of_completion: {
-    //   type: 'string',
-    //   description: 'Loose date field (as a string) for when the building was completed.',
-    // },
-    // style: {
-    //   type: 'string',
-    //   description:
-    //     'Brief statement about the architectural style. (Include more details in the body.)',
-    // },
-    // architect: {
-    //   type: 'string',
-    //   description: 'Name of the original architect.',
-    // },
-    // associate_architect: {
-    //   type: 'string',
-    //   description: 'Additional architect(s) on the original design.',
-    // },
-    // contractor: {
-    //   type: 'string',
-    //   description: 'Name of the original contractor.',
-    // },
-    // original_function: {
-    //   type: 'string',
-    //   description: 'How the build was first used.',
-    // },
-    // significance: {
-    //   type: 'string',
-    //   description: 'What makes the building significant?',
-    // },
-    // historic_status: {
-    //   type: 'string',
-    //   description: 'If it is a registered historic building, note here.',
-    // },
-    // unique_features: {
-    //   type: 'string',
-    //   description: 'Additional interesting features not captured in other fields.',
-    // },
-    // renovations: {
-    //   type: 'string',
-    //   description: 'Brief descriptions of renovation(s) done to the property.',
-    // },
-    // renovation_date: {
-    //   type: 'string',
-    //   description: 'When the renovation(s) were completed.',
-    // },
-    // renovation_architect: {
-    //   type: 'string',
-    //   description: 'Architect name for the renovation(s).',
-    // },
-    // renovation_style: {
-    //   type: 'string',
-    //   description: 'Explanation of the style of renovation(s).',
-    // },
-    // renovation_changes: {
-    //   type: 'string',
-    //   description: 'What changed in the renovation(s)?',
-    // },
-    // public_access: {
-    //   type: 'string',
-    //   description: 'How and which parts of the building can be accessed by the public.',
-    // },
-    // resources: {
-    //   type: 'string',
-    //   description: 'URL(s) to additional resources for the building.',
-    // },
-    // green_building_features: {
-    //   type: 'string',
-    //   description: 'Note LEED status, and other sustainability significance.',
-    // },
-    // quotes: {
-    //   type: 'string',
-    //   description: 'Quotes about the building.',
-    // },
-    // photo_credit: {
-    //   type: 'string',
-    //   description: 'If the images used for the building required attribution, add that here.',
-    // },
+    date_of_completion: {
+      type: 'string',
+      description: 'Loose date field (as a string) for when the building was completed.',
+    },
     static_map: {
       type: 'string',
       description:
@@ -234,26 +153,5 @@ export const Building = defineDocumentType(() => ({
       description: 'URL segment used to build dynamic paths in Astro template.',
       resolve: (building) => building._raw.sourceFileName.replace(/\.md$/, ''),
     },
-    // hasPrimaryAttributes: {
-    //   type: 'boolean',
-    //   description:
-    //     'Whether the building contains any of the primary attributes to be shown on the detail page.',
-    //   resolve: (building) =>
-    //     building.historic_status || building.current_owner || building.unique_features
-    //       ? true
-    //       : false,
-    // },
-    // hasSecondaryAttributes: {
-    //   type: 'boolean',
-    //   description:
-    //     'Whether the building contains any of the secondary attributes to be shown on the detail page.',
-    //   resolve: (building) =>
-    //     building.date_of_completion ||
-    //     building.original_owner ||
-    //     building.architect ||
-    //     building.style
-    //       ? true
-    //       : false,
-    // },
   },
 }))
