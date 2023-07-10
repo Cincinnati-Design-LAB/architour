fefbfb0139eae753b3d635c220c15665cd0bd6aa
Sean C Davis <scdavis41@gmail.com>
2023-07-10 06:47:03 -0400

    Minor transformation adjustments

--- content/schema/Building.transformer.ts
+++ content/schema/Building.transformer.ts
@@ -3,16 +3,25 @@ import { ROOT_DIR } from '@/content/utils/constants';
 import path from 'path';
 
 export async function transformBuilding(raw: RawBuilding, filePath: string): Promise<Building> {
+  // Name is the title in the source file
+  const name = raw.title;
+  // Create slug using the filename
   const slug = path.basename(filePath, path.extname(filePath));
+  // Stackbit ID is the relative path to the file from the root of the project
+  const stackbitId = path.relative(ROOT_DIR, filePath);
+  // URL path is the slug prefixed with `/buildings/`
+  const urlPath = `/buildings/${slug}`;
+  // Draft is true unless explicitly set to false in the source file
+  const draft = raw.draft === false ? false : true;
 
   const building: Building = {
-    name: raw.title,
+    draft,
+    name,
     slug,
-    stackbitId: path.relative(ROOT_DIR, filePath),
-    urlPath: `/buildings/${slug}`,
-
+    stackbitId,
     // TODO
     tourCount: 0,
+    urlPath,
 
     // stackbitId: raw.stackbitId,
     // urlPath: raw.urlPath,
