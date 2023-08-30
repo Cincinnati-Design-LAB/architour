eb37b600c2994b59368785b214009cec78514083
Sean C Davis <scdavis41@gmail.com>
2023-08-30 19:38:04 -0400

    Log out env vars

--- content/utils/cloudinary.ts
+++ content/utils/cloudinary.ts
@@ -4,6 +4,12 @@ import Cloudinary from 'cloudinary';
 
 const cloudinary = Cloudinary.v2;
 
+console.log({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
